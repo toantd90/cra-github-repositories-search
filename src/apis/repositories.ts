91d82f7c587a90636264fdd40dc350b13181fd8e
Toan Tran <toantdfu@gmail.com>
2023-01-31 18:15:05 +0900

    useThrottle when fetching data

--- src/apis/repositories.ts
+++ src/apis/repositories.ts
@@ -1,12 +1,15 @@
+import { Response } from 'Repository-Type';
+
 const GITHUB_API = 'https://api.github.com/search/repositories';
-const NUMBER_OF_REPOSITORIES_PER_PAGE = 10;
+export const NUMBER_OF_REPOSITORIES_PER_PAGE = 10;
 
 export async function fetchRepositories(
   query: string,
   page: number,
   perPage: number = NUMBER_OF_REPOSITORIES_PER_PAGE,
-) {
-  if (!query) return;
+): Promise<Response> {
+  console.log(`fetchRepositories: query: ${query}, page: ${page}`);
+  if (!query) return { items: [], total_count: 0 };
   const response = await fetch(
     `${GITHUB_API}?q=${query}&per_page=${perPage}&page=${page}`,
   );
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,6 +1,8 @@
-import { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import {
   Button,
+  Center,
+  CircularProgress,
   FormControl,
   FormLabel,
   Input,
@@ -12,50 +14,31 @@ import {
   UnorderedList,
 } from '@chakra-ui/react';
 import { ArrowBackIcon, ArrowForwardIcon, StarIcon } from '@chakra-ui/icons';
-import { useQuery, useQueryClient } from 'react-query';
 
 import { CircleIcon } from 'components';
 
-import { fetchRepositories } from 'apis/repositories';
+import useReactQuery from './useReactQuery';
+import useThrottle from './useThrottle';
+
+import { Repository } from 'Repository-Type';
 
 import styles from './styles.module.scss';
-
-const maxPostPage = 1;
+import { NUMBER_OF_REPOSITORIES_PER_PAGE } from 'apis/repositories';
 
 const HomePage = () => {
-  const queryClient = useQueryClient();
-  const [value, setValue] = useState('');
-  const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
+  const throttleSearchQuery = useThrottle(searchQuery);
 
-  useEffect(() => {
-    if (currentPage < maxPostPage) {
-      const nextPage = currentPage + 1;
-      queryClient.prefetchQuery(['posts', nextPage], () =>
-        fetchRepositories(value, nextPage),
-      );
-    }
-  }, [currentPage, queryClient, value]);
+  const { repositories, totalCount, isLoading, currentPage, setCurrentPage } =
+    useReactQuery(throttleSearchQuery);
 
-  const { data, error, isError, isLoading, isPreviousData } = useQuery(
-    ['repositories', currentPage],
-    () => fetchRepositories(value, currentPage),
-    {
-      keepPreviousData: true,
-      staleTime: 2000,
-    },
-  );
-
-  if (isLoading) {
-    return <span>Loading ...</span>;
-  }
-
-  if (isError) {
-    return <span>Error: </span>;
-  }
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
+  const onChangeHandle = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
   };
+
+  const hasMore = totalCount > currentPage * NUMBER_OF_REPOSITORIES_PER_PAGE;
+
+  console.log({ totalCount, hasMore, currentPage });
 
   return (
     <div className={styles.container}>
@@ -63,62 +46,71 @@ const HomePage = () => {
         <FormLabel>Github repository name</FormLabel>
         <Input
           placeholder='Input Github repository name'
-          onChange={handleChange}
-          value={value}
+          onChange={onChangeHandle}
         />
       </FormControl>
-
+      {isLoading && (
+        <Center>
+          <CircularProgress isIndeterminate color='green.300' />
+        </Center>
+      )}
       <UnorderedList className={styles.repositoriesContainer} spacing={6}>
-        {data?.items.map((item: any, index: number) => (
+        {repositories.map((item: Repository) => (
           <ListItem className={styles.repositoryWrapper} key={item.id}>
             <Link color='teal.400' href='#' fontWeight='bold' fontSize={16}>
               {item.full_name}
             </Link>
-            <Text fontSize={14} mb={1}>
+            <Text fontSize={14} mb={1} className={styles.description}>
               {item.description}
             </Text>
-            {item.topics.map((topic: string) => (
-              <Tag className={styles.topicTag} fontSize={12} mb={1}>
+            {item.topics.map((topic: string, index: number) => (
+              <Tag
+                className={styles.topicTag}
+                fontSize={12}
+                mb={1}
+                key={`${topic}-${index}`}
+              >
                 {topic}
               </Tag>
             ))}
             <Stack direction='row' alignItems='center' fontSize={12}>
               <StarIcon />
               <Text>{item.stargazers_count}</Text>
-              <CircleIcon /> <Text>{item.language}</Text>
+              {item.language && (
+                <>
+                  <CircleIcon /> <Text>{item.language}</Text>
+                </>
+              )}
             </Stack>
           </ListItem>
         ))}
       </UnorderedList>
-
-      <Stack
-        spacing={4}
-        direction='row'
-        align='center'
-        justify='flex-end'
-        className={styles.pages}
-      >
-        <Button
-          disabled={currentPage === 1}
-          onClick={() => setCurrentPage(currentPage - 1)}
-          leftIcon={<ArrowBackIcon />}
+      {repositories.length > 0 && (
+        <Stack
+          spacing={4}
+          direction='row'
+          align='center'
+          justify='flex-end'
+          className={styles.pages}
         >
-          Previous
-        </Button>
+          <Button
+            isDisabled={currentPage === 1}
+            onClick={() => setCurrentPage(currentPage - 1)}
+            leftIcon={<ArrowBackIcon />}
+          >
+            Previous
+          </Button>
 
-        <span>{currentPage}</span>
-        <Button
-          disabled={isPreviousData || currentPage === maxPostPage}
-          onClick={() => {
-            if (!isPreviousData) {
-              setCurrentPage(currentPage + 1);
-            }
-          }}
-          rightIcon={<ArrowForwardIcon />}
-        >
-          Next
-        </Button>
-      </Stack>
+          <span>{currentPage}</span>
+          <Button
+            isDisabled={!hasMore}
+            onClick={() => setCurrentPage(currentPage + 1)}
+            rightIcon={<ArrowForwardIcon />}
+          >
+            Next
+          </Button>
+        </Stack>
+      )}
     </div>
   );
 };
